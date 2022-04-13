0a4ce6f42ea213ee47286f836e4a1e5b7abbb667
NickA02 <69744811+NickA02@users.noreply.github.com>
2022-04-12 20:19:37 -0400

    just a few small changes

--- public/main.js
+++ public/main.js
@@ -1,7 +1,32 @@
 // Focus div based on nav button click
+prevDIV = document.getElementById("home")
 
+function showhome(id) {
+    if (prevDIV != null) {
+        prevDIV.className = "hidden"
+    }
+    var div = document.getElementById(id)
+    div.className = "active"
+    prevDIV = div
+}
 // Flip one coin and show coin image to match result when button clicked
-
+// Button coin flip element
+const coin = document.getElementById("quarter")
+coin.className = "smallcoin"
+// Add event listener for coin button
+coin.addEventListener("click", flipCoin)
+function flipCoin() {
+    fetch('http://localhost:5555/app/flip/', {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(result) {
+        console.log(result);
+        document.getElementById("result").innerHTML = result.flip;
+        document.getElementById("quarter").setAttribute("src", "./assets/img/" + result.flip+".png");
+        coin.disabled = true
+    })
+}
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
 
--- src/services/database.js
+++ src/services/database.js
@@ -1,1 +1,26 @@
-// Put your database code here
+// This ensures that things do not fail silently but will throw errors instead.
+"use strict";
+// Require better-sqlite.
+const Database = require('better-sqlite3');
+
+// Connect to a database or create one if it doesn't exist yet.
+const db = new Database('./data/db/log.db');
+
+// Is the database initialized or do we need to initialize it?
+const stmt = db.prepare(`
+    SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`
+    );
+// Define row using `get()` from better-sqlite3
+let row = stmt.get();
+// Check if there is a table. If row is undefined then no table exists.
+if (row === undefined) {
+// Set a const that will contain your SQL commands to initialize the database.
+    const sqlInit = `
+        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr VARCHAR, remoteuser VARCHAR, time VARCHAR, method VARCHAR, url VARCHAR, protocol VARCHAR,
+         httpversion NUMERIC, status INTEGER, referer TEXT, useragent TEXT);
+    `;
+// Execute SQL commands that we just wrote above.
+    db.exec(sqlInit);
+}
+// Export all of the above as a module so that we can use it elsewhere.
+module.exports = db
